78bd9c9f9f58d10ca80d194f3e907dbfc3c20cff
Atilist <71230342+Atilist@users.noreply.github.com>
2024-12-06 23:09:05 +0100

    Dirt and Farmland Requirements

--- mods/th_placement_rules/scripts/soil_requirements.lua
+++ mods/th_placement_rules/scripts/soil_requirements.lua
@@ -4,3 +4,17 @@ PlacementRules.sand_requirement = function (itemstack, placer, pointed_thing)
 		return core.item_place(itemstack, placer, pointed_thing)
 	end
 end
+
+PlacementRules.dirt_requirement = function (itemstack, placer, pointed_thing)
+    local node = core.get_node(pointed_thing.under)
+	if core.get_node_group(node.name, "soil") == 2 then
+		return core.item_place(itemstack, placer, pointed_thing)
+	end
+end
+
+PlacementRules.farmland_requirement = function (itemstack, placer, pointed_thing)
+    local node = core.get_node(pointed_thing.under)
+	if core.get_node_group(node.name, "soil") == 3 then
+		return core.item_place(itemstack, placer, pointed_thing)
+	end
+end
