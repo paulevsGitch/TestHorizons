03e415a9bbd092bff7ceaf2e5e83086351be249a
Atilist <71230342+Atilist@users.noreply.github.com>
2024-12-06 23:20:52 +0100

    Fix Indentation

--- mods/th_overworld/scripts/small_cactus.lua
+++ mods/th_overworld/scripts/small_cactus.lua
@@ -4,11 +4,11 @@ local function register_small_cactus(name)
 	local node_name = "th_overworld:" .. name
 
 	core.register_node(node_name, {
-        drawtype = "plantlike",
+		drawtype = "plantlike",
 		on_place = PlacementRules.sand_requirement,
 		description = translate(string.snake_to_title(name)),
 		tiles = {"th_" .. name .. ".png"},
-        paramtype = "light",
+		paramtype = "light",
 		groups = { plant = 1 }
 	})
 end
--- mods/th_placement_rules/scripts/soil_requirements.lua
+++ mods/th_placement_rules/scripts/soil_requirements.lua
@@ -1,19 +1,19 @@
 PlacementRules.sand_requirement = function (itemstack, placer, pointed_thing)
-    local node = core.get_node(pointed_thing.under)
+	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 1 then
 		return core.item_place(itemstack, placer, pointed_thing)
 	end
 end
 
 PlacementRules.dirt_requirement = function (itemstack, placer, pointed_thing)
-    local node = core.get_node(pointed_thing.under)
+	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 2 then
 		return core.item_place(itemstack, placer, pointed_thing)
 	end
 end
 
 PlacementRules.farmland_requirement = function (itemstack, placer, pointed_thing)
-    local node = core.get_node(pointed_thing.under)
+	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 3 then
 		return core.item_place(itemstack, placer, pointed_thing)
 	end
