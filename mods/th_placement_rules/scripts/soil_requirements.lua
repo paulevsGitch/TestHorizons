f9d1572f9bb98067700d054cef8ae98b78969bbf
Atilist <71230342+Atilist@users.noreply.github.com>
2024-12-06 23:27:54 +0100

    Documentation for Soil Requirements

--- mods/th_placement_rules/scripts/soil_requirements.lua
+++ mods/th_placement_rules/scripts/soil_requirements.lua
@@ -1,3 +1,5 @@
+--- A node with this rule can only be placed on soil of quality 1 or above.
+--- Function is named after sand as an example of quality 1 soil.
 PlacementRules.sand_requirement = function (itemstack, placer, pointed_thing)
 	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 1 then
@@ -5,6 +7,8 @@ PlacementRules.sand_requirement = function (itemstack, placer, pointed_thing)
 	end
 end
 
+--- A node with this rule can only be placed on soil of quality 2 or above.
+--- Function is named after dirt as an example of quality 2 soil.
 PlacementRules.dirt_requirement = function (itemstack, placer, pointed_thing)
 	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 2 then
@@ -12,6 +16,8 @@ PlacementRules.dirt_requirement = function (itemstack, placer, pointed_thing)
 	end
 end
 
+--- A node with this rule can only be placed on soil of quality 3 or above.
+--- Function is named after farmland as an example of quality 3 soil.
 PlacementRules.farmland_requirement = function (itemstack, placer, pointed_thing)
 	local node = core.get_node(pointed_thing.under)
 	if core.get_node_group(node.name, "soil") >= 3 then
